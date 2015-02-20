294e38b06de97f5ddb7b52b20339f2034ff31dd0
Mike Wallace <mikewallace@apache.org>
2015-02-20 13:49:09 +0000

    Remove cruft, add test
    Strip out the random sending of message behaviour so all workers
    do is receive messages and add a test that sent messages are
    receieved.

---
+++ tests.py
@@ -0,0 +1,41 @@
+import unittest
+
+from collections import deque
+from multiprocessing import Process, Queue
+
+from noedz import send, worker
+
+_WORKERS = 3
+
+class TestNoedz(unittest.TestCase):
+    def setUp(self):
+        self.procs = deque()
+        self.queues = {}
+        self.debug_queues = {}
+        for pid in range(0, _WORKERS):
+            self.queues[pid] = Queue()
+            self.debug_queues[pid] = Queue()
+        for pid in range(0, _WORKERS):
+            p = Process(
+                target=worker,
+                args=(pid, self.queues),
+                kwargs={"debug": self.debug_queues[pid]}
+            )
+            p.start()
+            self.procs.append(p)
+
+    def tearDown(self):
+        for p in self.procs:
+            p.terminate()
+
+    def testMessage(self):
+        target_worker = 0
+        sender_pid = -1
+        test_msg = 'Message from {0}'.format(sender_pid)
+        send(test_msg, sender_pid, self.queues[target_worker])
+        msg = self.debug_queues[target_worker].get(timeout=5)
+        assert msg == '{0} receieved message from {1}: {2}'.format(
+            target_worker,
+            sender_pid,
+            test_msg
+        )
